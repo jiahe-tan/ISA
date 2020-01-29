47a55c564e2015211afbac0e3f6d35d3f0a1746c
Jiahe <jt7gu@virginia.edu>
2020-01-29 16:57:50 -0500

    add users

--- isa/urls.py
+++ isa/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from main.views import send_json
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('sendjson/', send_json, name='send_json'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 
+from .models import *
 # Create your views here.
+
+def user_create(request):
+    if request.method == 'POST':
+        json_data = request.POST
+        user = Users()
+        for k, v in json_data.items():
+            setattr(user, key, value)
+        user.save()
+        return JsonResponse(model_to_dict(userObj))
+    else:
+        return HttpResponse("error")
+
+def user_info(request, user_id):
+    if request.method == 'POST':
+        json_data = request.POST
+        user = Users.objects.get(pk=user_id)
+        if 'username' in json_data:
+            user.username = json_data['username']
+        if 'email' in json_data:
+            user.email = json_data['email']
+        if 'location' in json_data:
+            user.location = json_data['location']
+        user.save()
+        return JsonResponse(model_to_dict(user)) 
+    else:
+        return HttpResponse("error")
+
+def user_all(request):
+    if request.method == 'GET':
+        users = Users.objects.all()
+        usersList = []
+        for i in users:
+            usersList.append(model_to_dict(i))
+        return JsonResponse(usersList, safe=False)
+    else:
+        return HttpResponse("error")
+
+def send_json(request):
+
+    data = [{'name': 'Peter', 'email': 'peter@example.org'},
+            {'name': 'Julia', 'email': 'julia@example.org'}]
+
+    return HttpResponse("error")
